806d43cea52d31239fea557b1d4dfd68ab21e46b
unknown <aziz706797@gmail.com>
2023-09-15 12:40:27 +0600

    create home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Navbar from '@/scenes/navbar';
+import Home from "@/scenes/home";
 import {useState, useEffect} from 'react'
 import { SelectedPage } from '@/shared/types';
 
@@ -24,6 +25,10 @@ const App = () => {
         isTopOfPage={isTopOfPage}
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
+          />
+          <Home
+           setSelectedPage={setSelectedPage}
+          
           />
   </div>;
 };
